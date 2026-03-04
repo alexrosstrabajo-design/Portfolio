d9ff5dd8d087d8466407cc0cc999e2947750f514
alexrosstrabajo-design <alexrosstrabajo@gmail.com>
2026-03-04 08:25:22 -0400

    back to normal

--- src/App.jsx
+++ src/App.jsx
@@ -57,7 +57,7 @@ export default function App() {
         beamWidth={1}
         beamHeight={30}
         beamNumber={30}
-        lightColor="#5faffa"
+        lightColor="#ffffff"
         speed={2}
         noiseIntensity={4}
         scale={0.31}
