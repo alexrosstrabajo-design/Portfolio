8411341425302e7dc52252360e25ca84a4e713b3
Alexander <alexrosstrabajo@gmail.com>
2026-03-03 21:39:26 -0400

    Change beam light color to white

--- src/App.jsx
+++ src/App.jsx
@@ -57,7 +57,7 @@ export default function App() {
         beamWidth={1}
         beamHeight={30}
         beamNumber={30}
-        lightColor="#00ff00"
+        lightColor="#ffffff"
         speed={2}
         noiseIntensity={4}
         scale={0.31}
