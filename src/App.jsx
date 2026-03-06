091f659b2b8c3493bc32b0c580dfc3b55da22ff3
alexrosstrabajo-design <alexrosstrabajo@gmail.com>
2026-03-06 08:14:48 -0400

    fondo rojo

--- src/App.jsx
+++ src/App.jsx
@@ -57,7 +57,7 @@ export default function App() {
         beamWidth={1}
         beamHeight={30}
         beamNumber={30}
-        lightColor="#ffffff"
+        lightColor="#f20000"
         speed={2}
         noiseIntensity={4}
         scale={0.31}
